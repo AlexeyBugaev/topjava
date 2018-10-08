b8129a6e3a14ace9070bde65dae59d078fd887c2
AlexeyBugaev <alexeybugaevjava@gmail.com>
2018-10-08 22:54:32 +0700

    Prepare to HW1

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web;
 import org.slf4j.Logger;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,16 +25,15 @@ public class MealServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
-        List<MealWithExceed> mealWithExceeds = Arrays.asList(
-                new MealWithExceed(LocalDateTime.of(2018, Month.OCTOBER, 8, 7, 0), "Завтрак", 500, false),
-                new MealWithExceed(LocalDateTime.of(2018, Month.OCTOBER, 8, 12, 0), "Обед", 1000, false),
-                new MealWithExceed(LocalDateTime.of(2018, Month.OCTOBER, 8, 19, 0), "Ужин", 500, false),
-                new MealWithExceed(LocalDateTime.of(2018, Month.OCTOBER, 9, 7, 0), "Завтрак", 1000,true),
-                new MealWithExceed(LocalDateTime.of(2018, Month.OCTOBER, 9, 12, 0), "Обед", 500,true),
-                new MealWithExceed(LocalDateTime.of(2018, Month.OCTOBER, 9, 19, 0), "Ужин", 700,true)
+        List<Meal> meals = Arrays.asList(
+                new Meal(LocalDateTime.of(2018, Month.OCTOBER, 8, 7, 0), "Завтрак", 500),
+                new Meal(LocalDateTime.of(2018, Month.OCTOBER, 8, 12, 0), "Обед", 1000),
+                new Meal(LocalDateTime.of(2018, Month.OCTOBER, 8, 19, 0), "Ужин", 500),
+                new Meal(LocalDateTime.of(2018, Month.OCTOBER, 9, 7, 0), "Завтрак", 1000),
+                new Meal(LocalDateTime.of(2018, Month.OCTOBER, 9, 12, 0), "Обед", 500),
+                new Meal(LocalDateTime.of(2018, Month.OCTOBER, 9, 19, 0), "Ужин", 700)
         );
-        request.setAttribute("meals", mealWithExceeds);
-
+        request.setAttribute("meals", MealsUtil.getFilteredWithExceededByCycle(meals, LocalTime.of(0,0),LocalTime.of(23,59),2000));
         request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
